5da8f26b3536dd6409ff833ccb08f670e043bdcb
Brad Schrag <brad.schrag@gmail.com>
2019-01-11 06:20:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -19,7 +19,7 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/add/)
-      
+      new_item = req.path
     else
       resp.write "Path Not Found"
     end
